8dfa6f6b7853a300d1181062bbb0e22437e03a63
rtm713 <rtm713@gmail.com>
2023-04-04 00:19:41 -0500

    added starter code and character arrays

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+// Assignment code here
+
+var lowCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var uppCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var special = ["!","@","#","$","%",]
+var numbers = ["0","1","2","3","4","5","6","7","8","9"]
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
