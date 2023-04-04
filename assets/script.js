efcbb4b4c9f1feec81c5d73f77c56c8920c4b4c5
rtm713 <rtm713@gmail.com>
2023-04-04 18:04:26 -0500

    completed while loops and added error messages

--- assets/script.js
+++ assets/script.js
@@ -5,40 +5,103 @@ var uppCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","
 var special = ["!","@","#","$","%",]
 var numbers = ["0","1","2","3","4","5","6","7","8","9"]
 
+
+function generatePassword() {
+  var passPool = [];
+  var password ="";
+  var charTypeCheck = 0;
+
+  var passLen = prompt("How many characters? (8-128)");
+  Number(passLen);
+
+  while ((passLen < 8) || (passLen > 128)) {
+    alert("Error: number must be between 8-128")
+    var passLen = prompt("How many characters? (8-128)");
+    Number(passLen);
+  }
+
+  var useLow = confirm("Do you want to include lowercase letters?")
+  var useUpp = confirm("Do you want to include Uppercase letters?")
+  var useSpec = confirm("Do you want to include special characters? (@,%,!)")
+  var useNum = confirm("Do you want to include Numbers?")
+
+  if (useLow === true) {
+    passPool = passPool.concat(lowCase);
+    password = password + lowCase[Math.floor(Math.random() * lowCase.length)];
+    passLen = passLen - 1;
+    charTypeCheck++;
+  }
+  if (useUpp === true) {
+    passPool = passPool.concat(uppCase);
+    password = password + uppCase[Math.floor(Math.random() * uppCase.length)];
+    passLen = passLen - 1;
+    charTypeCheck++;
+  }
+  if (useSpec === true) {
+    passPool = passPool.concat(special);
+    password = password + special[Math.floor(Math.random() * special.length)];
+    passLen = passLen - 1;
+    charTypeCheck++;
+  }
+  if (useNum === true) {
+    passPool = passPool.concat(numbers);
+    password = password + numbers[Math.floor(Math.random() * numbers.length)];
+    passLen = passLen - 1;
+    charTypeCheck++;
+  }
+
+  while (charTypeCheck===0) {
+    alert("Error: You Must choose at least one character type for your password. (uppercase, lowercase, special characters, or numbers)")
+    var useLow = confirm("Do you want to include lowercase letters?")
+    var useUpp = confirm("Do you want to include Uppercase letters?")
+    var useSpec = confirm("Do you want to include special characters? (@,%,!)")
+    var useNum = confirm("Do you want to include Numbers?")
+
+    if (useLow === true) {
+      passPool = passPool.concat(lowCase);
+      password = password + lowCase[Math.floor(Math.random() * lowCase.length)];
+      passLen = passLen - 1;
+      charTypeCheck++;
+    }
+    if (useUpp === true) {
+      passPool = passPool.concat(uppCase);
+      password = password + uppCase[Math.floor(Math.random() * uppCase.length)];
+      passLen = passLen - 1;
+      charTypeCheck++;
+    }
+    if (useSpec === true) {
+      passPool = passPool.concat(special);
+      password = password + special[Math.floor(Math.random() * special.length)];
+      passLen = passLen - 1;
+      charTypeCheck++;
+    }
+    if (useNum === true) {
+      passPool = passPool.concat(numbers);
+      password = password + numbers[Math.floor(Math.random() * numbers.length)];
+      passLen = passLen - 1;
+      charTypeCheck++;
+    }
+  }
+
+
+
+  for (i=0; i<passLen; i++) {
+    password = password + passPool[Math.floor(Math.random() * passPool.length)]
+  }
+
+  return password
+}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
 
-  var passLen = prompt("How many characters? (8-128)");
-  Number(passLen);
-  var useLow = confirm("Do you want to include lowercase letters?")
-  var useUpp = confirm("Do you want to include Uppercase letters?")
-  var useSpec = confirm("Do you want to include special characters? (@,%,!)")
-  var useNum = confirm("Do you want to include Numbers?")
-
-  var passPool = []
-
-  if (useLow === true) {passPool = passPool.concat(lowCase);}
-  if (useUpp === true) {passPool = passPool.concat(uppCase);}
-  if (useSpec === true) {passPool = passPool.concat(special);}
-  if (useNum === true) {passPool = passPool.concat(numbers);}
-
-  for (i=0; i<passLen; i++) {
-
-  }; 
-
-  /*
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-  function generatePassword() {
-  }
-
-*/
 }
 
 // Add event listener to generate button
